e7405a5f9d4e83ef005132c07422a983da4df8c6
Natalie Dodd <nataliedodd89@gmail.com>
2017-11-18 15:35:41 +0100

    added react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log("Bonjourrrr!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(App),
+    document.getElementById('mount')
+  );
+});
