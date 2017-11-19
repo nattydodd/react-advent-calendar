ea294d195f2feed76bb69dcd6950326520960eec
Natalie Dodd <nataliedodd89@gmail.com>
2017-11-19 13:01:39 +0100

    styled door handle and hinges

--- components/door.js
+++ components/door.js
@@ -22,8 +22,11 @@ class Door extends Component {
       <div
         className="door__frame">
         <div
-          className={`door__door ${this.state.open ? "door__door--open" : ""}`}
+          className={`door__frame__door ${this.state.open ? "door__frame__door--open" : ""}`}
           onClick={() => this.toggleDoor()}>
+          <div className="door__frame__door__handle"></div>
+          <div className="door__frame__door__hinge top"></div>
+          <div className="door__frame__door__hinge bottom"></div>
         </div>
       </div>
     );
