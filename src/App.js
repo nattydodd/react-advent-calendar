4f08b67b5d9b5e4d0248200cb5c7ebb5bf94fd60
Natalie Dodd <nataliedodd89@gmail.com>
2017-11-28 22:05:14 +0100

    added basic routes for calendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Component } from 'react';
 import Calendar from '../components/calendar.js';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Calendar />
-      </div>
+      <Router>
+        <div className="app">
+          <Link to={`calendar`}>
+            Calendar
+          </Link>
+          <Route path="/calendar" component={Calendar} />
+        </div>
+      </Router>
     );
   }
 }
